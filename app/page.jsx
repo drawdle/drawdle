be09eb35c2bbf48e65b245fd2d4c6699da20e46b
ChakornK <chakornk2007@gmail.com>
2023-11-17 10:11:02 -0800

    fix color

--- app/page.jsx
+++ app/page.jsx
@@ -7,13 +7,13 @@ export default function Home() {
     <>
       <NavBar />
       <main className="flex min-h-screen flex-col items-center justify-between bg-beige-800">
-        <div className="bg-beige-300 h-[70vh] w-full flex items-center justify-center flex-col">
+        <div className="bg-beige-400 h-[70vh] w-full flex items-center justify-center flex-col">
           <p className="text-5xl text-beige-900">Draw-dle</p>
           <p className="text-beige-700">Learn to draw by completing doodles</p>
           <Link href={"/draw"}>
-          <button className="mt-8 rounded-full bg-beige-800 px-8 py-3 text-2xl text-beige-300">
-            Start
-          </button>
+            <button className="mt-8 rounded-full bg-beige-800 px-8 py-3 text-2xl text-beige-300">
+              Start
+            </button>
           </Link>
         </div>
         <div className="max-w-[860px] w-full flex flex-col gap-2 px-16 py-12 text-beige-100">
