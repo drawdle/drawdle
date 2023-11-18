1c517ffbc7c7d0169f353d3fff01d1e705886b7c
ChakornK <chakornk2007@gmail.com>
2023-11-18 12:37:23 -0800

    bug fixes

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between bg-beige-800">
         <div
           id="header"
-          className="transition-[translate_margin] duration-0 bg-beige-500 h-[70vh] w-full flex items-center justify-center flex-col"
+          className="transition-[translate,margin] duration-0 bg-beige-500 h-[70vh] min-h-[300px] w-full pt-16 flex items-center justify-center flex-col"
         >
           <p className="text-5xl text-beige-900">Draw-dle</p>
           <p className="text-beige-700">Learn to draw by completing doodles</p>
@@ -52,7 +52,7 @@ export default function Home() {
         ></div>
         <div
           id="body"
-          className="transition-[translate_margin] duration-0 max-w-[860px] w-full flex flex-col gap-2 px-16 py-12 text-beige-100"
+          className="transition-[translate,margin] duration-0 max-w-[860px] w-full flex flex-col gap-2 px-16 py-12 text-beige-100"
         >
           <p className="text-2xl text-beige-200">About us</p>
           <p>lorem ipsum dolor sit amet </p>
