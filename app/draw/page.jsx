f551ce9189217cb9d6db5a30f077bca6d4b1a345
ChakornK <chakornk2007@gmail.com>
2023-11-27 12:06:07 -0800

    improve intro transition

--- app/draw/page.jsx
+++ app/draw/page.jsx
@@ -3,23 +3,23 @@ import React from "react";
 import DrawingCanvas from "../components/canvas";
 import { NavBar } from "../components/navbar";
 
+import "./introTransition.css";
+
 export default class Draw extends React.Component {
   componentDidMount() {
     if (new URL(window.location.href).searchParams.get("transition")) {
       window.history.pushState({}, "", "/draw");
-      document.getElementById("canvasContainer").style.opacity = "0";
-      document.getElementById("canvasContainer").style.transition =
-        "opacity 450ms ease-in-out";
+      document
+        .getElementById("canvasContainer")
+        .classList.add("introTransition");
+      document.querySelector("main").classList.add("introTransition");
     }
-    setTimeout(() => {
-      document.getElementById("canvasContainer").style.opacity = "";
-    });
   }
   render() {
     return (
       <>
         <NavBar />
-        <main className="bg-beige-200 min-h-[100vh]">
+        <main className="bg-beige-200 h-[100vh]">
           <div id="canvasContainer">
             <DrawingCanvas />
           </div>
