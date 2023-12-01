d085bc60aaa063a4c48cf503f1559c64223b88fe
ChakornK <chakornk2007@gmail.com>
2023-12-01 11:10:33 -0800

    forgot to export functions

--- app/utils/colors.js
+++ app/utils/colors.js
@@ -6,7 +6,7 @@
  * @param {number} l - The lightness value (0-1).
  * @returns {[number, number, number]} The HSV color as an array of [h, s, v].
  */
-function hsl2hsv(h, s, l) {
+export function hsl2hsv(h, s, l) {
   const v = s * Math.min(l, 1 - l) + l;
   return [h, v ? 2 - (2 * l) / v : 0, v];
 }
@@ -19,7 +19,7 @@ function hsl2hsv(h, s, l) {
  * @param {number} v - The value value (0-1).
  * @returns {[number, number, number]} - The HSL color representation.
  */
-function hsv2hsl(h, s, v) {
+export function hsv2hsl(h, s, v) {
   const l = v - (v * s) / 2;
   const m = Math.min(l, 1 - l);
 
@@ -34,7 +34,7 @@ function hsv2hsl(h, s, v) {
  * @param {number} v - The value value.
  * @return {[number, number, number]} The RGB color values.
  */
-function hsv2rgb(h, s, v) {
+export function hsv2rgb(h, s, v) {
   let f = (n, k = (n + h / 60) % 6) =>
     v - v * s * Math.max(Math.min(k, 4 - k, 1), 0);
   return [f(5), f(3), f(1)];
@@ -48,7 +48,7 @@ function hsv2rgb(h, s, v) {
  * @param {number} b - The blue color value (0-255).
  * @return {[number, number, number]} An array containing the hue, saturation, and value of the color.
  */
-function rgb2hsv(r, g, b) {
+export function rgb2hsv(r, g, b) {
   let v = Math.max(r, g, b),
     c = v - Math.min(r, g, b);
   let h =
@@ -64,7 +64,7 @@ function rgb2hsv(r, g, b) {
  * @param {number} l - The lightness value of the color (0-1).
  * @return {[number, number, number]} An array representing the RGB values of the converted color.
  */
-function hsl2rgb(h, s, l) {
+export function hsl2rgb(h, s, l) {
   let a = s * Math.min(l, 1 - l);
   let f = (n, k = (n + h / 30) % 12) =>
     l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
@@ -79,7 +79,7 @@ function hsl2rgb(h, s, l) {
  * @param {number} b - The blue color value (0-255).
  * @return {[number, number, number]} An array containing the HSL color values.
  */
-function rgb2hsl(r, g, b) {
+export function rgb2hsl(r, g, b) {
   let v = Math.max(r, g, b),
     c = v - Math.min(r, g, b),
     f = 1 - Math.abs(v + v - c - 1);
