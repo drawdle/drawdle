b5bdb6604fe769ce4378eb31effcc2292be41011
ChakornK <chakornk2007@gmail.com>
2024-09-04 13:57:29 -0700

    line smoothing with bezier curve

--- app/draw/draw.ts
+++ app/draw/draw.ts
@@ -13,6 +13,8 @@ const params = {
 	eraserSize: 5,
 };
 
+const tension = 1;
+
 let _setPanMode = (_: "pan-zoom" | "none") => {};
 export function setPanMode(mode: "pan-zoom" | "none") {
 	_setPanMode(mode);
@@ -109,12 +111,37 @@ function main(viewport: Viewport) {
 	container.addChild(paper);
 
 	// drawing
+	let points: { x: number; y: number }[] = [];
 	const drawingLayer = new PIXI.Container();
 	container.addChild(drawingLayer);
 	const mask = new PIXI.Graphics().rect(-320, -240, 640, 480).fill(0xffffff);
 	mask.position.set(window.innerWidth / 2, window.innerHeight / 2);
 	drawingLayer.addChild(mask);
 	drawingLayer.mask = mask;
+
+	const drawLine = () => {
+		const line = drawingLayer.children.findLast(
+			(c) => c instanceof PIXI.Graphics
+		) as PIXI.Graphics;
+		line.clear();
+		line.moveTo(points[0].x, points[0].y);
+		// https://stackoverflow.com/a/49371349
+		for (let i = 1; i < points.length - 1; i++) {
+			const p0 = i > 0 ? points[i - 1] : points[0];
+			const p1 = points[i];
+			const p2 = points[i + 1];
+			const p3 = i !== points.length - 2 ? points[i + 2] : p2;
+
+			const cp1x = p1.x + ((p2.x - p0.x) / 6) * tension;
+			const cp1y = p1.y + ((p2.y - p0.y) / 6) * tension;
+
+			const cp2x = p2.x - ((p3.x - p1.x) / 6) * tension;
+			const cp2y = p2.y - ((p3.y - p1.y) / 6) * tension;
+
+			line.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, p2.x, p2.y);
+		}
+		line.stroke();
+	};
 
 	viewport.interactive = true;
 	let isPointerDown = false;
@@ -124,19 +151,20 @@ function main(viewport: Viewport) {
 		isPointerDown = true;
 		if (["brush", "eraser"].includes(params.tool)) {
 			drawingLayer.addChild(
-				new PIXI.Graphics()
-					.setStrokeStyle({
-						color: params.color,
-						cap: "round",
-						join: "round",
-						width:
-							params.tool === "eraser" ? params.eraserSize : params.brushSize,
-					})
-					.moveTo(
-						(e.clientX - viewport.x) / viewport.scale.x,
-						(e.clientY - viewport.y) / viewport.scale.y
-					)
+				new PIXI.Graphics().setStrokeStyle({
+					color: params.color,
+					cap: "round",
+					join: "round",
+					width:
+						params.tool === "eraser" ? params.eraserSize : params.brushSize,
+				})
 			);
+			points = [
+				{
+					x: (e.clientX - viewport.x) / viewport.scale.x,
+					y: (e.clientY - viewport.y) / viewport.scale.y,
+				},
+			];
 		}
 	});
 	viewport.addEventListener("pointerup", (e) => {
@@ -146,15 +174,11 @@ function main(viewport: Viewport) {
 		if (params.isPanning) return;
 
 		if (["brush", "eraser"].includes(params.tool) && isPointerDown) {
-			const currentLine = drawingLayer.children.findLast(
-				(c) => c instanceof PIXI.Graphics
-			) as PIXI.Graphics;
-			currentLine
-				.lineTo(
-					(e.clientX - viewport.x) / viewport.scale.x,
-					(e.clientY - viewport.y) / viewport.scale.y
-				)
-				.stroke();
+			points.push({
+				x: (e.clientX - viewport.x) / viewport.scale.x,
+				y: (e.clientY - viewport.y) / viewport.scale.y,
+			});
+			drawLine();
 		}
 	});
 }
