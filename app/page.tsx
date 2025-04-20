a1ab8f386ba3ad9294446a4f85b22498168e0bc6
ChakornK <chakornk2007@gmail.com>
2025-04-20 02:20:16 -0700

    feat: add preview on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<main className="flex flex-col justify-start items-stretch bg-beige-800 min-h-screen text-beige-200">
 			<div className="flex flex-col justify-center items-center gap-3 bg-beige-500 h-96 text-beige-900">
 				<h1 className="text-5xl">Drawdle</h1>
-				<p>Learn to draw by completing scribbles</p>
+				<p>Learn to draw</p>
 				<Link
 					href={"/draw"}
 					className="bg-beige-800 px-6 py-2 rounded-full text-beige-200 text-xl"
@@ -13,9 +13,9 @@ export default function Home() {
 					Start
 				</Link>
 			</div>
-			<div className="flex-col justify-start items-start p-24">
-				<h2 className="text-2xl">About us</h2>
-				<p>Lorem ipsum</p>
+			<div className="flex flex-col justify-start items-start gap-4 p-24">
+				<h2 className="text-2xl">Preview</h2>
+				<img className="shadow-lg border border-beige-400 rounded-2xl" src="/preview.jpg" alt="Preview" />
 			</div>
 		</main>
 	);
